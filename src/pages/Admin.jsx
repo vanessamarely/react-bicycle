7fadc2b68e88ecc182d115d4e1d9676e58fb4132
vanessamarely <vanessamarely@gmail.com>
2023-10-30 19:56:51 -0500

    updating url

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -29,7 +29,7 @@ const Admin = () => {
     errorDelete: errorDeleteERC,
     deleteData: deleteDataERC,
   } = useDelete(urlGetAllCER, urlGetAllCERService);
-
+  const [dataRentAll, setDataRentAll] = useState([]);
   const { response: eventsData } = useFetch(urlGetAllCER, urlGetAllCERService);
 
   const [optionSelected, setOptionSelected] = useState("rent");
@@ -51,7 +51,15 @@ const Admin = () => {
     deleteDataERC({ eventId: e.eventId });
   };
 
- 
+  useEffect(() => {
+    if (!rentData) {
+      setDataRentAll([]);
+    } else {
+      if (rentData.length > 0) {
+        setDataRentAll(rentData);
+      }
+    }
+  }, [rentData]);
 
   return (
     <div className="w-full p-2">
@@ -167,31 +175,32 @@ const Admin = () => {
               </div>
             )}
             <div className="flex flex-wrap w-full h-96 overflow-y-auto">
-              {rentData && rentData?.map((rent) => (
-                <div
-                  key={rent?.rentId}
-                  className="max-w-sm p-2 m-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-                >
-                  <div className="block py-2 pl-3 pr-4 text-indigo-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-indigo-500 md:dark:hover:text-indigo-500 dark:hover:bg-gray-700 dark:hover:text-indigo-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                    <svg
-                      className="w-6 h-6 text-gray-800 dark:text-white"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 14 20"
-                    >
-                      <path d="M7 0a7 7 0 0 0-1 13.92V19a1 1 0 1 0 2 0v-5.08A7 7 0 0 0 7 0Zm0 5.5A1.5 1.5 0 0 0 5.5 7a1 1 0 0 1-2 0A3.5 3.5 0 0 1 7 3.5a1 1 0 0 1 0 2Z" />
-                    </svg>
+              {dataRentAll &&
+                dataRentAll?.map((rent) => (
+                  <div
+                    key={rent?.rentId}
+                    className="max-w-sm p-2 m-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <div className="block py-2 pl-3 pr-4 text-indigo-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-indigo-500 md:dark:hover:text-indigo-500 dark:hover:bg-gray-700 dark:hover:text-indigo-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                      <svg
+                        className="w-6 h-6 text-gray-800 dark:text-white"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 14 20"
+                      >
+                        <path d="M7 0a7 7 0 0 0-1 13.92V19a1 1 0 1 0 2 0v-5.08A7 7 0 0 0 7 0Zm0 5.5A1.5 1.5 0 0 0 5.5 7a1 1 0 0 1-2 0A3.5 3.5 0 0 1 7 3.5a1 1 0 0 1 0 2Z" />
+                      </svg>
 
-                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                      Bicycle ID: {rent?.bicycleId}
-                    </h5>
+                      <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                        Bicycle ID: {rent?.bicycleId}
+                      </h5>
 
-                    <p>Rent Date: {rent?.rentDate}</p>
-                    <p>Return Date: {rent?.returnDate}</p>
+                      <p>Rent Date: {rent?.rentDate}</p>
+                      <p>Return Date: {rent?.returnDate}</p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         )}
